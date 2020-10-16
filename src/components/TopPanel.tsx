426a354684eba66cd1a8e7d28d93f75da2b910eb
EldarMuhamethanov <eldarmy@mail.ru>
2020-10-16 10:21:32 +0300

    Top panel

--- src/components/TopPanel.tsx
+++ src/components/TopPanel.tsx
@@ -1,7 +1,12 @@
-
+import React from 'react';
+import './TopPanel.css';
 
 function TopPanel() {
-
+    return(
+        <div className="top-panel">
+            
+        </div>
+    )
 }
 
 export {
